bf736fbd4bc93c84a30a3ca8e07956f700562b7d
kirannaik212 <49430683+kirannaik212@users.noreply.github.com>
2019-04-10 11:32:33 +0530

    Update TestSorting.java

--- src/main/java/com/scp/sorting/TestSorting.java
+++ src/main/java/com/scp/sorting/TestSorting.java
@@ -5,7 +5,7 @@ package com.scp.sorting;
 public class TestSorting {
 		public static void main(String[] args) {
 			int i=10,j=20;
-			int c=i+j;
+			
 			
 			System.out.println("my first repo   .....");
 			System.out.println("my first repo   .....");
