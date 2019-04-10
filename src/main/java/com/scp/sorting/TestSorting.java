654bc4a963ddb474dd6588041ea8129775ded6e0
kirannaik212 <49430683+kirannaik212@users.noreply.github.com>
2019-04-10 11:29:31 +0530

    Update TestSorting.java

--- src/main/java/com/scp/sorting/TestSorting.java
+++ src/main/java/com/scp/sorting/TestSorting.java
@@ -11,5 +11,5 @@ public class TestSorting {
 			System.out.println("my first repo   .....");
 			System.out.println("my first repo   .....");
 			System.out.println("my first repo   .....");
-			System.out.println("my first repo   .....");
+			
 		}}
