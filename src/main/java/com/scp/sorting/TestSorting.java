e35646b3f45d849e96c4247595f246824a15b3af
kiran <kiran@DESKTOP-P0N7KLO>
2019-04-10 11:12:08 +0530

    my first commit

--- src/main/java/com/scp/sorting/TestSorting.java
+++ src/main/java/com/scp/sorting/TestSorting.java
@@ -5,5 +5,5 @@ package com.scp.sorting;
 public class TestSorting {
 		public static void main(String[] args) {
 			
-			System.out.println("my first repo");
+			System.out.println("my first repo   .....");
 		}}
